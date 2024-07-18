7babb9116616187598b66f8db6ee9df652a061a5
random name <60777462+jjjllxx@users.noreply.github.com>
2024-07-18 09:17:17 +0800

    Update HJ108.py

--- HJ108.py
+++ HJ108.py
@@ -1,12 +1,3 @@
-"""
-File: HJ108.py
-Author: Jin Lexuan
-E-mail: jlx321@126.com
-Time: 2021-07-29 23:04:55
-Function:
-
-
-"""
 m, n = map(int, input().split(' '))
 a = 1
 for i in range(1, min(m, n)+1):
