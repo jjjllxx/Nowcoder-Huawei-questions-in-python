daeb15bbb32d94d23e2bcb1371edfec80a7a117d
random name <60777462+jjjllxx@users.noreply.github.com>
2024-08-16 09:28:34 +0800

    Update HJ61.py

--- HJ61.py
+++ HJ61.py
@@ -1,14 +1,3 @@
-"""
-File: HJ61.py
-Author: Jin Lexuan
-E-mail: jlx321@126.com
-Time: 2022-01-13 01:11:12
-Function:
-
-
-"""
-
-
 def apple0(apple, plate):
     if apple < 0 or plate < 0:
         return 0
@@ -16,7 +5,6 @@ def apple0(apple, plate):
         return 1
     else:
         return apple0(apple, plate - 1) + apple0(apple - plate, plate)
-
 
 while True:
     try:
