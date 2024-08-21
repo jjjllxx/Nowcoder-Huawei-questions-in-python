9af7f1d18f342dc4d9dbd5a707a9df7c7d93a04a
random name <60777462+jjjllxx@users.noreply.github.com>
2024-08-21 12:57:13 +0800

    Update HJ73.py

--- HJ73.py
+++ HJ73.py
@@ -1,13 +1,3 @@
-"""
-File: HJ73.py
-Author: Jin Lexuan
-E-mail: jlx321@126.com
-Time: 2021-07-28 23:48:23
-Function:
-
-
-"""
-
 m_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 y, m, d = map(int, input().split(' '))
 num = 0
@@ -17,4 +7,3 @@ num += d
 if ((y % 4 == 0 and y % 100 != 0) or y % 400 == 0) and m >= 3:
     num += 1
 print(num)
-
