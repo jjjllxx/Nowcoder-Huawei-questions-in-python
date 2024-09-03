99f89461027cb4ce9c7e709ec33d03550c88371f
random name <60777462+jjjllxx@users.noreply.github.com>
2024-09-03 09:15:48 +0800

    Update HJ86.py

--- HJ86.py
+++ HJ86.py
@@ -1,12 +1,3 @@
-"""
-File: HJ86.py
-Author: Jin Lexuan
-E-mail: jlx321@126.com
-Time: 2021-07-29 00:27:16
-Function:
-
-
-"""
 while True:
     n = str(bin(int(input())))
     num, num_max = 0, 0
@@ -18,4 +9,3 @@ while True:
         else:
             num = 0
     print(num_max)
-
